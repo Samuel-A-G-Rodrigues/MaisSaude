04dafe84dfd56a6fdc12b79d8b9566051a8075e7
Samuel-A-G-Rodrigues <samuelaureliogr@gmail.com>
2020-04-11 23:15:26 -0300

    Criar Propiedades, Contrutor e Metodos da Classe RefeicaoPrevista

--- src/maissaude/RefeicaoPrevista.java
+++ src/maissaude/RefeicaoPrevista.java
@@ -5,10 +5,95 @@
  */
 package maissaude;
 
+import java.sql.Time;
+import java.util.Objects;
+
 /**
  *
  * @author Samuel Rodrigues
  */
 public class RefeicaoPrevista {
+    
+    private Time horRefeicao;
+    private String nomRefeicao;
+    private Double valCalorico;
+
+    public RefeicaoPrevista(Time horRefeicao, String nomRefeicao, Double valCalorico) {
+
+        this.horRefeicao = horRefeicao;
+        this.nomRefeicao = nomRefeicao;
+        this.valCalorico = valCalorico;
+    }
+
+    /**
+     * @return the horRefeicao
+     */
+    public Time getHorRefeicao() {
+        return horRefeicao;
+    }
+
+    /**
+     * @param horRefeicao the horRefeicao to set
+     */
+    public void setHorRefeicao(Time horRefeicao) {
+        this.horRefeicao = horRefeicao;
+    }
+
+    /**
+     * @return the nomRefeicao
+     */
+    public String getNomRefeicao() {
+        return nomRefeicao;
+    }
+
+    /**
+     * @param nomRefeicao the nomRefeicao to set
+     */
+    public void setNomRefeicao(String nomRefeicao) {
+        this.nomRefeicao = nomRefeicao;
+    }
+
+    /**
+     * @return the valCalorico
+     */
+    public Double getValCalorico() {
+        return valCalorico;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(horRefeicao + ", " + nomRefeicao + ", " + valCalorico + ".");
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + Objects.hashCode(this.horRefeicao);
+        hash = 47 * hash + Objects.hashCode(this.nomRefeicao);
+        hash = 47 * hash + Objects.hashCode(this.valCalorico);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RefeicaoPrevista other = (RefeicaoPrevista) obj;
+        if (!Objects.equals(this.horRefeicao, other.horRefeicao)) {
+            return false;
+        }
+        if (!Objects.equals(this.nomRefeicao, other.nomRefeicao)) {
+            return false;
+        }
+        if (!Objects.equals(this.valCalorico, other.valCalorico)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
