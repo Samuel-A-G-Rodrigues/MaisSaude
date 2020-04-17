65a8dba6b0b1a2aa4ce5fb6e6e8761b9c5c73855
GUILHERME BARBOSA <gui.diascampos@outlook.com>
2020-04-16 22:47:30 -0300

    adicionando classes

---
+++ src/maissaude/Rotina.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maissaude;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ *
+ * @author Havai
+ */
+public class Rotina {
+
+    private int valorCalorico;
+    private Double numMeta;
+    private boolean metaAlcancada;
+
+    /**
+     * @return the valorCalorico
+     */
+    public Rotina(int valorCalorico, double numMeta, boolean metaAlcancada) {
+
+        this.valorCalorico = valorCalorico;
+        this.numMeta = numMeta;
+        this.metaAlcancada = metaAlcancada;
+    }
+
+    public int getValorCalorico() {
+        return valorCalorico;
+    }
+
+    /**
+     * @param valorCalorico the valorCalorico to set
+     */
+    public void setValorCalorico(int valorCalorico) {
+        this.valorCalorico = valorCalorico;
+    }
+
+    /**
+     * @return the numMeta
+     */
+    public Double getNumMeta() {
+        return numMeta;
+    }
+
+    /**
+     * @param numMeta the numMeta to set
+     */
+    public void setNumMeta(Double numMeta) {
+        this.numMeta = numMeta;
+    }
+
+    /**
+     * @return the metaAlcancada
+     */
+    public boolean isMetaAlcancada() {
+        return metaAlcancada;
+    }
+
+    /**
+     * @param metaAlcancada the metaAlcancada to set
+     */
+    public void setMetaAlcancada(boolean metaAlcancada) {
+        this.metaAlcancada = metaAlcancada;
+    }
+
+    //Metodos
+    public String cadastrarLembrete(String nomLembrete, Date data, String desLembrete) {
+        Lembrete lembrete = new Lembrete(nomLembrete, data, desLembrete, false);
+        return lembrete.toString();
+    }
+
+    public String cadastrarRefeicaoPrevista(Time horRefeicao, String nomRefeicao, Double valCalorico) {
+        RefeicaoPrevista refeicao = new RefeicaoPrevista(horRefeicao, nomRefeicao, valCalorico);
+        return refeicao.toString();
+    }
+
+    public String cadastrarRefeicaoFeita(String desRefeicao, String nomRefeicao, Double valCalorico) {
+        RefeicaoFeita refeicao = new RefeicaoFeita(desRefeicao, nomRefeicao, valCalorico);
+        return refeicao.toString();
+    }
+
+    public String informacaoNutricional(Double valInformacao, String nomInformacao) {
+        InformacaoNutricional infoNutricional = new InformacaoNutricional(valInformacao, nomInformacao);
+        return infoNutricional.toString();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(numMeta + ", " + metaAlcancada + ", ");
+    }
+}
