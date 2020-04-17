9d7e889f106c8e686e07cbdf16d0b1d45fe79f64
mathbatista2501 <User@DESKTOP-F2AQ8KM>
2020-04-17 20:31:11 -0300

    CRIAR CLASSE ALIMENTO

---
+++ src/maissaude/Alimento.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maissaude;
+
+/**
+ *
+ * @author User
+ */
+public class Alimento {
+    private String nomAlimento;
+    private Double valCalorico;
+    private String desAlimento;
+    private Double desPeso;
+    
+}
